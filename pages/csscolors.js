09d4a3339ac1cc5adfb8c86b0419e09e89871f64
Jacob <jacobmoyle@gmail.com>
2019-11-21 02:45:06 -0800

    Theme: Setup system based fonts. Remove copy pasta from csscolors page

--- pages/csscolors.js
+++ pages/csscolors.js
@@ -2,10 +2,8 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-import MuiLink from '@material-ui/core/Link'
 import ProTip from '../src/ProTip'
 import Link from '../src/Link'
-import { Button } from '@material-ui/core'
 
 export default function CssColors() {
   return (
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,26 @@ import { createMuiTheme } from '@material-ui/core/styles'
 import { red, lightBlue, blueGrey } from '@material-ui/core/colors'
 
 const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    h1: { fontSize: '4rem', fontWeight: 700 },
+    h2: { fontSize: '2.125rem', fontWeight: 500 },
+    h3: { fontSize: '1.625rem', fontWeight: 600 },
+    h4: { fontSize: '1.5rem', fontWeight: 500 },
+    h5: { fontSize: '1.3rem' },
+    button: { textTransform: 'none' },
+  },
   palette: {
     primary: {
       main: lightBlue[500],
